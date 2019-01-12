5bec776a6709fc16f06f5445922c9084d3bca156
Denys Fridman <denys.fridman@deepsense.ai>
2019-01-12 11:14:40 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='algorithms-coursera',
+    version='0.0.1',
+    description='Stanford Algorithms Course Hosted On Coursera',
+    url='https://github.com/dfridman1/algorithms-coursera',
+    packages=find_packages(include=['algorithms-coursera']),
+    install_requires=[
+    ],
+)
